3e36092c9ac6b8a5b68ceb3ae3a6d9bc56db4428
allthesignals <wilburnforce@gmail.com>
2016-08-09 02:59:44 -0400

    Now supports pivoting

--- routes/infogram.js
+++ routes/infogram.js
@@ -1,21 +1,21 @@
 var express = require('express');
 var router = express.Router();
 var CartoDB = require('cartodb');
-
+var d3 = require('d3');
 
 
 function convertToInfogram(data) {
-  var arrays = [[]];
 
+  // data = data.rows.entries();
+  var arrays = [[]];
   var core = arrays[0];
   var series = [];
   var firstRow = data.rows[0];
-  // first collection series names
-  // series.push("dataset");
-  for (i in firstRow) {
-    if (firstRow.hasOwnProperty(i)) {
+
+  for (var [i,k] of firstRow) {
+    // if (firstRow.hasOwnProperty(i)) {
       series.push(i);
-    }
+    // }
   }
 
   core.push(series);
@@ -24,16 +24,46 @@ function convertToInfogram(data) {
     var core = arrays[0];
     var collection = [];
 
-    for (i in row) {
-      if (row.hasOwnProperty(i)) {
-        collection.push(row[i]);
-      }
+    for (var [i,k] of row) {
+      // if (row.hasOwnProperty(i)) {
+        collection.push(row.get(i));
+      // }
     }
     core.push(collection);
   });
   
   return arrays;
   
+}
+
+function cast(data, groupBy, aggrBy, val) {
+  var result = [];
+  var nestedData = d3.nest()
+                  .key(function(d) { return d[groupBy]; })
+                  .entries(data.rows);
+  
+
+  nestedData.forEach(function(each) {
+    var obj = {};
+    var map = new Map();
+    obj[groupBy] = each.key;
+    map.set(groupBy, each.key);
+    
+    each.values.forEach(function(d) {
+      map.set(d[aggrBy], d[val]);
+      obj[d[aggrBy]] = d[val];
+    });
+    
+    delete each.values;
+    delete each.key;
+
+    console.log(map);
+
+    result.push(map);
+  });
+  
+  return { rows: result };
+
 }
 
 /* GET users listing. */
@@ -43,9 +73,28 @@ router.get('/', function(req, res, next) {
 
   var sql = new CartoDB.SQL({user:user});
   sql.execute(req.query.sql)
-    //you can listen for 'done' and 'error' promise events
     .done(function(data) { 
-      res.send(convertToInfogram(data));//data.rows is an array with an object for each row in your result set
+      var groupBy = req.query.groupBy,
+          aggrBy = req.query.aggrBy, 
+          val = req.query.val;
+
+      if (groupBy && aggrBy && val) { data = cast(data, groupBy, aggrBy, val); }
+      else { 
+        var map = new Map(),
+            result = [];
+        data.rows.forEach(function(row) {
+          for (i in row) {
+            if (row.hasOwnProperty(i)) {
+              map.set(i, row[i]);    
+            }
+          }
+          result.push(map);
+          
+        });
+        data = { rows: result }
+      }
+        console.log(data);
+      res.send(convertToInfogram(data));
     })
     .error(function(error) {
       res.send(error);
